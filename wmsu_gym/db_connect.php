98f9f11a2bf5c97e5272529bb7645027ada39fdc
bryanjamesgarcia <ae202402448@wmsu.edu.ph>
2025-12-15 23:11:16 +0800

    Update db_connect.php

--- wmsu_gym/db_connect.php
+++ wmsu_gym/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "db_wmsu_gym";
+$servername = "sql104.infinityfree.com";
+$username = "if0_40677566";
+$password = "bryanjames0906";
+$database = "if0_40677566_db_wmsu_gym";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
